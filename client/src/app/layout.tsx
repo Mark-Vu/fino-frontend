61829e9af8c9ea011bb7c8099e706bf5a5de6a57
MinhVD16 <markvu2003@gmail.com>
2026-06-30 16:04:56 -0700

    add force dynamic

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -18,6 +18,8 @@ const geistMono = Geist_Mono({
     subsets: ["latin"],
 });
 
+export const dynamic = "force-dynamic";
+
 export const metadata: Metadata = {
     title: "Fino | Accounting Tools - Bank Statement, Delivery Receipt Converter",
     description:
